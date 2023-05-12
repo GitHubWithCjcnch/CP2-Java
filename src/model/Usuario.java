5440d0a8e295b9591789565cd01498717e3ecfd5
Caio Vitor <githubwithcjcnch@gmail.com>
2023-05-12 00:07:44 -0300

    commit 2

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -2,17 +2,59 @@ package model;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
-public class Usuario {
-	int id;
-	String nome;
-	String email;
-	String senha;
-	Date loginDate = new Date();
-	DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy HH:mm:ss");
-	String dateFormatted = dateFormat.format(loginDate);
+public class Usuario extends Conta {
+	private int id;
+	private String nome;
+	private String email;
+	private Date loginDate = new Date();
+	private DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy HH:mm:ss");
+	private String dateFormatted = dateFormat.format(loginDate);
 	
+	public Usuario(String username, String senha, String nome, String email) {
+		super(username, senha);
+		this.nome = nome;
+		this.email = email;
+	}
 	
+	public void menuUsuario() {
+		Scanner objInput = new Scanner(System.in);
+		while(this.isUserLogged()) {
+			System.out.println("_________________________________________________");
+			System.out.println("Bem Vindo " + this.nome + "               Email: " + this.email);
+			System.out.println("Saldo: " + this.getSaldo());
+			System.out.println("");
+			System.out.println("Selecione uma opção:");
+            System.out.println("1 - Visualizar extrato de transações");
+            System.out.println("2 - Visualizar meus investimentos");
+            System.out.println("3 - Realizar transferência bancária");
+            System.out.println("4 - Realizar pagamento de boleto");
+            System.out.println("5 - Realizar investimento");
+            System.out.println("6 - Acessar serviços de assessoria financeira");
+            System.out.println("0 - Sair");
+            System.out.println("_________________________________________________");
+            int opcao = objInput.nextInt();
+            
+            switch(opcao) {
+            	case 1:
+            		
+            		break;
+            	case 2:
+            		// visualizar extrato de transações
+            		break;
+            	case 3:
+            		// realizar transferência bancária
+            		break;
+            	case 4:
+            		// realizar pagamento de boleto
+            		break;
+            	case 5:
+            		// realizar investimento
+            		break;
+            }
+		}
+	}
 	
 	public int getId() {
 		return id;
@@ -32,12 +74,7 @@ public class Usuario {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+	
 	public String getDateFormatted() {
 		return dateFormatted;
 	}	
--- src/view/main.java
+++ src/view/main.java
@@ -2,30 +2,44 @@ package view;
 
 import java.util.Scanner;
 
+import model.Usuario;
+import model.Administrador;
+import model.Sistema;
+
 public class main {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		Scanner objInput = new Scanner(System.in);
+		Usuario usuario1 = new Usuario("usuario1", "senha123", "Paulo", "Paulo@gmail.com");
+		Usuario usuario2 = new Usuario("usuario2", "senha456", "Rogerio", "Rogerio@gmail.com");
+		Usuario usuario3 = new Usuario("usuario3", "senha789", "Sandra", "Sandra@gmail.com");
+		Administrador admin = new Administrador("admin", "admin");
+		
 		System.out.println("                    BEM VINDO                    ");
 		System.out.println("_________________________________________________");
-		System.out.println("Realize o login no sistema:");
-		System.out.println("Email: ");
-		String emailUser = objInput.next();
-		System.out.println("Senha: ");
-		String senhaUser = objInput.next();
-		
-		
-		
-		int optionUser = objInput.nextInt();
-		switch(optionUser) {
-			case 1:
-				// blabla
+		System.out.println("                    LOGIN                    ");
+		System.out.println("_________________________________________________");
+		while(true) {
+			System.out.println("Usuário: ");
+			String emailUser = objInput.next();
+			System.out.println("Senha: ");
+			String senhaUser = objInput.next();
+			if (usuario1.login(emailUser, senhaUser)) {
+				usuario1.menuUsuario();
 				break;
-			case 2:
-				// blabla
+			} else if (usuario2.login(emailUser, senhaUser)) {
+				usuario2.menuUsuario();
 				break;
-			case 3:
-				// blabla
+			} else if (usuario3.login(emailUser, senhaUser)) {
+				usuario3.menuUsuario();
 				break;
+			} else if (admin.login(emailUser, senhaUser)) {
+				admin.menuAdmin();
+				break;
+			} else {
+				System.out.println("Usuário ou senha inválidos.");
+				continue;
+			}
 		}
+		objInput.close();
 	}
 }

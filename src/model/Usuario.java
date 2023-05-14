df09ef40e667f3c261825f3794a6d8ddbbe386d4
Caio Vitor <githubwithcjcnch@gmail.com>
2023-05-14 18:54:16 -0300

    Last commit

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -6,54 +6,131 @@ import java.util.Scanner;
 
 public class Usuario extends Conta {
 	private int id;
+	private Scanner objInput;
 	private String nome;
 	private String email;
+	private boolean isLogado;
 	private Date loginDate = new Date();
 	private DateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy HH:mm:ss");
 	private String dateFormatted = dateFormat.format(loginDate);
 	
-	public Usuario(String username, String senha, String nome, String email) {
+	public Usuario(String username, String senha, String nome, String email, Scanner objInput) {
 		super(username, senha);
 		this.nome = nome;
+		this.objInput = objInput;
 		this.email = email;
 	}
 	
-	public void menuUsuario() {
-		Scanner objInput = new Scanner(System.in);
-		while(this.isUserLogged()) {
-			System.out.println("_________________________________________________");
+	public void exibirMenu() {
+		setLogado(true);
+		while(Sistema.isUserLogged()) {
+			System.out.println("--------------------------------------------------");
 			System.out.println("Bem Vindo " + this.nome + "               Email: " + this.email);
 			System.out.println("Saldo: " + this.getSaldo());
 			System.out.println("");
 			System.out.println("Selecione uma opção:");
-            System.out.println("1 - Visualizar extrato de transações");
-            System.out.println("2 - Visualizar meus investimentos");
+			System.out.println("1 - Depósitar/Sacar dinheiro");
+            System.out.println("2 - Visualizar extrato de transações");
             System.out.println("3 - Realizar transferência bancária");
-            System.out.println("4 - Realizar pagamento de boleto");
-            System.out.println("5 - Realizar investimento");
-            System.out.println("6 - Acessar serviços de assessoria financeira");
+            System.out.println("4 - Realizar investimento");
+            System.out.println("5 - Visualizar meus investimentos");
+            System.out.println("6 - Realizar pagamento de boleto");
+            System.out.println("7 - Acessar serviços de assessoria financeira");
             System.out.println("0 - Sair");
-            System.out.println("_________________________________________________");
+            System.out.println("--------------------------------------------------");
             int opcao = objInput.nextInt();
             
             switch(opcao) {
-            	case 1:
-            		
+            	case 0:
+            		this.logout();
+            		setLogado(false);
             		break;
+            	case 1:
+            		System.out.println("Selecione uma opção:");
+        			System.out.println("1 - Depositar dinheiro");
+                    System.out.println("2 - Sacar dinheiro");
+                    opcao = objInput.nextInt();
+                    if (opcao == 1) {
+                    	System.out.println("Qual o valor de depósito? ");
+                    	double valor = objInput.nextDouble();
+                    	System.out.println("");
+                    	this.depositar(valor);
+                		System.out.println("Seu saldo atual: " + this.getSaldo());
+                    } else if (opcao == 2) {
+                    	System.out.println("Qual o valor de saque? ");
+                    	double valor = objInput.nextDouble();
+                    	System.out.println("");
+                    	this.sacar(valor);
+            			System.out.println("Seu saldo atual: " + this.getSaldo());
+                    } else {
+                    	System.out.println("Valor Incorreto.");
+                    }
+                    break;
             	case 2:
-            		// visualizar extrato de transações
+            		this.visualizarExtrato();
             		break;
             	case 3:
-            		// realizar transferência bancária
+            		System.out.println("Para qual conta você deseja enviar? Digite o email da conta");
+            		String email = objInput.next();
+            		System.out.println("Digite o valor que você deseja transferir: ");
+            		double valor = objInput.nextDouble();
+            		this.transferenciaBancaria(valor, email);
             		break;
             	case 4:
-            		// realizar pagamento de boleto
+            		System.out.println("Onde você deseja realizar o seu investimento? ");
+            		System.out.println("1 - Investimento 1 (Rentabilidade de 3,11% - RISCO III)");
+                    System.out.println("2 - Investimento 2 (Rentabilidade de 2,50% - RISCO II)");
+                    System.out.println("3 - Investimento 3 (Rentabilidade de 1,10% - RISCO I)");
+                    opcao = objInput.nextInt();
+                    if (opcao == 1) {
+                        System.out.println("Qual o valor: ");
+                		double valorInvestimento = objInput.nextDouble();
+                    	this.realizarInvestimento(valorInvestimento, 0.0311, "Risco III");
+                    } else if (opcao == 2) {
+                    	System.out.println("Qual o valor: ");
+                		double valorInvestimento = objInput.nextDouble();
+                    	this.realizarInvestimento(valorInvestimento, 0.025, "Risco II");
+                    } else if (opcao == 3) {
+                    	System.out.println("Qual o valor: ");
+                		double valorInvestimento = objInput.nextDouble();
+                    	this.realizarInvestimento(valorInvestimento, 0.0110, "Risco I");
+                    } else {
+                    	System.out.println("OPÇÃO INCORRETA! Escolha alguma das opções");
+                    }
             		break;
             	case 5:
+            		this.visualizarInvestimentos();
+            		break;
+            	case 6:
             		// realizar investimento
             		break;
-            }
+            	case 7:
+            		// realizar investimento
+            		break;
+           }
 		}
+	}
+	
+	
+	public void transferenciaBancaria(double valor, String emailDestino) {
+	    Conta contaDestino = this.buscarConta(emailDestino);
+	    if (contaDestino == null) {
+	        System.out.println("Conta de destino não encontrada");
+	        return;
+	    }
+	    if (contaDestino.getClass() != this.getClass()) {
+	        System.out.println("Apenas é possível realizar transferências entre contas do mesmo tipo");
+	        return;
+	    }
+	 	
+	 	if (this.getSaldo() < valor) {
+	        System.out.println("Saldo insuficiente");
+	        return;
+	    }
+
+	 	this.sacar(valor);
+	    contaDestino.depositar(valor);
+	    System.out.println("Transferência realizada com sucesso");
 	}
 	
 	public int getId() {
@@ -77,5 +154,13 @@ public class Usuario extends Conta {
 	
 	public String getDateFormatted() {
 		return dateFormatted;
+	}
+
+	public boolean isLogado() {
+		return isLogado;
+	}
+
+	public void setLogado(boolean isLogado) {
+		this.isLogado = isLogado;
 	}	
 }
--- src/view/main.java
+++ src/view/main.java
@@ -7,39 +7,37 @@ import model.Administrador;
 import model.Sistema;
 
 public class main {
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		Scanner objInput = new Scanner(System.in);
-		Usuario usuario1 = new Usuario("usuario1", "senha123", "Paulo", "Paulo@gmail.com");
-		Usuario usuario2 = new Usuario("usuario2", "senha456", "Rogerio", "Rogerio@gmail.com");
-		Usuario usuario3 = new Usuario("usuario3", "senha789", "Sandra", "Sandra@gmail.com");
-		Administrador admin = new Administrador("admin", "admin");
-		
-		System.out.println("                    BEM VINDO                    ");
-		System.out.println("_________________________________________________");
-		System.out.println("                    LOGIN                    ");
-		System.out.println("_________________________________________________");
+		Usuario usuario1 = new Usuario("usuario1", "senha123", "Paulo", "Paulo@gmail.com", objInput);
+		Usuario usuario2 = new Usuario("usuario2", "senha456", "Rogerio", "Rogerio@gmail.com", objInput);
+		Usuario usuario3 = new Usuario("usuario3", "senha789", "Sandra", "Sandra@gmail.com", objInput);
+		Administrador admin = new Administrador("admin", "admin", objInput);
+		Sistema.addConta(usuario1);
+	    Sistema.addConta(usuario2);
+	    Sistema.addConta(usuario3);
+	    Sistema.addConta(admin);
 		while(true) {
+			System.out.println("                    BEM VINDO                    ");
+			System.out.println("_________________________________________________");
+			System.out.println("                    LOGIN                    ");
+			System.out.println("_________________________________________________");
 			System.out.println("Usuário: ");
 			String emailUser = objInput.next();
 			System.out.println("Senha: ");
 			String senhaUser = objInput.next();
 			if (usuario1.login(emailUser, senhaUser)) {
-				usuario1.menuUsuario();
-				break;
+				usuario1.exibirMenu();
 			} else if (usuario2.login(emailUser, senhaUser)) {
-				usuario2.menuUsuario();
-				break;
+				usuario2.exibirMenu();
 			} else if (usuario3.login(emailUser, senhaUser)) {
-				usuario3.menuUsuario();
-				break;
+				usuario3.exibirMenu();
 			} else if (admin.login(emailUser, senhaUser)) {
-				admin.menuAdmin();
-				break;
+				admin.exibirMenu();
 			} else {
 				System.out.println("Usuário ou senha inválidos.");
 				continue;
 			}
 		}
-		objInput.close();
 	}
 }
